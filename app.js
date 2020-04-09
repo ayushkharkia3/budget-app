a9dc85919a1e969466f48f83dbc33e17d41e4f1c
Ayush Kharkia <ayushkharkia3@gmail.com>
2020-04-09 22:14:01 +0530

    hey1

--- app.js
+++ app.js
@@ -19,4 +19,6 @@ app.use((req,res,next)=>{
     res.status(404).render('404');
 })
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000,()=>{
+    console.log("App is running");
+});
